83bd8368a1e86b8089db694926c98d50f03e0c2f
s3897250 <s3897250@student.rmit.edu.au>
2023-12-12 13:01:43 +1100

    organised server.ts file for express routing

--- express/src/route/product.route.ts
+++ express/src/route/product.route.ts
@@ -1,11 +1,12 @@
-module.exports = (express, app) => {
-    const controller = require("../controller/product.controller.ts");
+import express, { Express } from 'express';
+const controller = require('../controller/product.controller.ts');
+
+export default (app: Express) => {
     const router = express.Router();
     
     // Create a product.
     router.post("/createProduct", controller.createProduct);
   
     // Add routes to server.
-    app.use("/api/reply", router);
-  };
-  
+    app.use("/api/product", router);
+};
--- express/src/server.ts
+++ express/src/server.ts
@@ -1,15 +1,24 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import productRoutes from './route/product.route.ts';
+const cors = require("cors");
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+// Add CORS suport.
+app.use(cors());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
+
+// Using routes
+productRoutes(app)
+
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
